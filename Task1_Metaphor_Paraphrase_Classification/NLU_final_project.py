c4022b057c1d29f7fee72effe4f70813666c529b
gruksd <sofia.gruk@helsinki.fi>
2024-05-30 15:35:47 +0300

    updated files

--- Task1_Metaphor_Paraphrase_Classification/NLU_final_project.py
+++ Task1_Metaphor_Paraphrase_Classification/NLU_final_project.py
@@ -2,48 +2,40 @@ import nltk
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-with open('metaphor_paraphrase_corpus.txt', 'r') as file:
-    data = file.read().strip()
+def test_data_f():
+    with open('metaphor_paraphrase_corpus.txt', 'r') as file:
+        data = file.read().strip()
 
-blocks = data.split('\n\n')
+    blocks = data.split('\n\n')
 
-test_data = []
+    test_data = []
 
-result_data = []
+    for block in blocks:
+        lines = [line.strip() for line in block.split("\n") if line.strip()]
+        if not lines:
+            continue  
 
-for block in blocks:
-    lines = [line.strip() for line in block.split("\n") if line.strip()]
-    if not lines:
-        continue  
+        premise = lines[0]
 
-    premise = lines[0]
+        for line in lines[1:]:
+            if '#' in line:
+                hypothesis, label = line.rsplit('#', 1)
+                label = int(label)
+                if label == 4 or label == 3:
+                    new_label = 0
+                if label == 2:
+                    new_label = 1
+                if label == 1:
+                    new_label = 2
+                test_data.append([premise, hypothesis.strip()])
+            else:
+                continue
+    return test_data
 
-    for line in lines[1:]:
-        if '#' in line:
-            hypothesis, label = line.rsplit('#', 1)
-            label = int(label)
-            if label == 4 or label == 3:
-                new_label = 0
-            if label == 2:
-                new_label = 1
-            if label == 1:
-                new_label = 2
-            result_data.append([premise, hypothesis.strip(), new_label])
-            test_data.append([premise, hypothesis.strip()])
-        else:
-            continue
-
-
-
-tokenizer = AutoTokenizer.from_pretrained("ixaxaar/flan-t5-base_multi-nli")
-model = AutoModelForSequenceClassification.from_pretrained("ixaxaar/flan-t5-base_multi-nli")
-
-
-label_map = {0: 0, 1: 1, 2: 2} #{0: "entailment", 1: "neutral", 2: "contradiction"}
-
-def classify_nli(premise, hypothesis):
+def classify_nli(tokenizer,model,premise, hypothesis):
     input_text = f"{premise} [SEP] {hypothesis}"
-
+    
+    label_map = {0: 0, 1: 1, 2: 2} #{0: "entailment", 1: "neutral", 2: "contradiction"}
     inputs = tokenizer(input_text, return_tensors="pt", truncation=True, padding=True)
 
     with torch.no_grad():
@@ -52,10 +44,34 @@ def classify_nli(premise, hypothesis):
     predicted_label = torch.argmax(logits, dim=-1).item()
     return label_map[predicted_label]
 
-with open("output_file.txt", 'w', encoding='utf-8') as file:
-        for premise, hypothesis in test_data:
-            result = classify_nli(premise, hypothesis)
-            file.write(f"{premise}#{hypothesis}#{result}\n")
+def nli_t5_test(test_data):
+    tokenizer = AutoTokenizer.from_pretrained("ixaxaar/flan-t5-base_multi-nli")
+    model = AutoModelForSequenceClassification.from_pretrained("ixaxaar/flan-t5-base_multi-nli")
 
+    with open("output_file_nli_t5.txt", 'w', encoding='utf-8') as file:
+            for premise, hypothesis in test_data:
+                result = classify_nli(tokenizer, model,premise, hypothesis)
+                file.write(f"{premise}#{hypothesis}#{result}\n")
 
+def nli_klue_roberta(test_data):
+    tokenizer = AutoTokenizer.from_pretrained("Huffon/klue-roberta-base-nli")
+    model = AutoModelForSequenceClassification.from_pretrained("Huffon/klue-roberta-base-nli")
 
+    with open("output_file_klue_roberta.txt", 'w', encoding='utf-8') as file:
+            for premise, hypothesis in test_data:
+                result = classify_nli(tokenizer, model, premise, hypothesis)
+                file.write(f"{premise}#{hypothesis}#{result}\n")
+
+def nli_large_roberta(test_data):
+    tokenizer = AutoTokenizer.from_pretrained("ynie/roberta-large-snli_mnli_fever_anli_R1_R2_R3-nli")
+    model = AutoModelForSequenceClassification.from_pretrained("ynie/roberta-large-snli_mnli_fever_anli_R1_R2_R3-nli")
+
+    with open("output_file_large_roberta.txt", 'w', encoding='utf-8') as file:
+            for premise, hypothesis in test_data:
+                result = classify_nli(tokenizer, model, premise, hypothesis)
+                file.write(f"{premise}#{hypothesis}#{result}\n")
+
+test_data = test_data_f()
+#nli_t5_test(test_data)
+nli_klue_roberta(test_data)
+nli_large_roberta(test_data)
--- Task1_Metaphor_Paraphrase_Classification/accuracy_nli.py
+++ Task1_Metaphor_Paraphrase_Classification/accuracy_nli.py
@@ -7,14 +7,18 @@ def read_labels(file_path):
                 labels.append(parts[-1])
     return labels
 
+def accuracy_f(true_labels_file, predicted_labels_file):
+    true_labels = read_labels(true_labels_file)
+    predicted_labels = read_labels(predicted_labels_file)
+    print(predicted_labels_file)
+    if len(true_labels) == len(predicted_labels):
+        matches = sum(1 for true, predicted in zip(true_labels, predicted_labels) if true == predicted)
+        accuracy = (matches / len(true_labels)) * 100
+        print(f"Accuracy: {accuracy:.2f}%")
+    else:
+        print("We are missing some labels:", len(predicted_labels), "/", len(true_labels))
+
 true_labels_file = 'test_suit.txt'
-predicted_labels_file = 'output_file.txt'
-
-true_labels = read_labels(true_labels_file)
-predicted_labels = read_labels(predicted_labels_file)
-
-matches = sum(1 for true, predicted in zip(true_labels, predicted_labels) if true == predicted)
-
-accuracy = (matches / len(true_labels)) * 100
-
-print(f"Accuracy: {accuracy:.2f}%")
+output_files = ['output_file.txt', "output_file_klue_roberta.txt", "output_file_large_roberta.txt"]
+for file in output_files:
+    accuracy_f(true_labels_file, file)
